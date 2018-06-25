a15d99712f0c626ccfc640b81801ef72729e1652
Kesava Karthik Kota <35501783+kesavakk@users.noreply.github.com>
2018-06-24 23:46:45 -0400

    Add files via upload
    fsdsafd

--- part3.py
+++ part3.py
@@ -4,5 +4,32 @@ import requests
 from bs4 import BeautifulSoup
 
 # write your code here
-# usage should be python3 part3.py
+michiganDailyUrl="https://www.michigandaily.com/"
 
+def scrapper (url):
+
+    page= requests.get(url)
+    pagecontent= page.content
+    soup= BeautifulSoup(pagecontent, 'html.parser')
+    return soup
+
+
+michigandailycontent = scrapper("https://www.michigandaily.com/")
+
+
+
+print("Michigan Daily -- MOST READ")
+mostReadDiv = michigandailycontent.find("div", attrs={'class' : 'view-most-read'}).find("ol").find_all("li")
+for item in mostReadDiv:
+
+    print(item.find("a").text.strip())
+
+    try:
+        linksoup =scrapper (michiganDailyUrl+item.find("a")['href'])
+        author= linksoup.find("div", attrs = {"class" : "byline"}).find('div').find('a').text.strip()
+        print("By " + author)
+    except:
+        linksoup =scrapper (michiganDailyUrl+item.find("a")['href'])
+        author= linksoup.find("p", attrs = {"class" : "info"}).contents[0]
+        print(author)
+    # print ("\n")
--- part4.py
+++ part4.py
@@ -1,6 +1,30 @@
 # Imports -- you may add others but do not need to
-import plotly.plotly as py
-import plotly.graph_objs as go
+import plotly
+import csv
+plotly.tools.set_credentials_file(username='kesava.k3', api_key='yHh05VCzbAdr20JPvm69')
+
 
 # Code here should involve creation of the bar chart as specified in instructions
 # And opening / using the CSV file you created earlier with noun data from tweets
+nouns=[]
+count=[]
+f = open("noun_data.csv", "r")
+
+reader = csv.reader(f)
+i = 0
+for line in reader:
+    if i > 0:
+        nouns.append(line[0])
+        count.append(line[1])
+    i+=1
+
+trace = plotly.graph_objs.Bar(x=nouns, y=count,
+    marker=dict(
+        color=['rgba(204,204,204,1)', 'rgba(222,45,38,0.8)',
+               'rgba(204,204,204,1)', 'rgba(204,204,204,1)',
+               'rgba(204,204,204,1)']), width = [0.8, 0.55, 0.5, 0.25, 0.1])
+data = [trace]
+layout = plotly.graph_objs.Layout(title='Nouns data', width=800, height=640)
+fig = plotly.graph_objs.Figure(data=data, layout=layout)
+
+plotly.plotly.image.save_as(fig, filename='part4_viz_image.png')
