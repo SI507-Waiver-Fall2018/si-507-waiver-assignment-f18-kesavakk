cd47cc2142105ad650084ce2a1b88b6ae02582d6
Kesava Karthik Kota <35501783+kesavakk@users.noreply.github.com>
2018-06-25 00:27:55 -0400

    Add files via upload

--- part2.py
+++ part2.py
@@ -2,74 +2,58 @@
 import sys
 import sqlite3
 
-# write your code here
-# usage should be
-#  python3 part2.py customers
-#  python3 part2.py employees
-#  python3 part2.py orders cust=<customer id>
-#  python3 part2.py orders emp=<employee last name>
-
-def createConnection(dbfile):
+def connection(database):
 	try:
-		conn = sqlite3.connect(dbfile)
-		return conn
+		connection = sqlite3.connect(database)
+		return connection
 	except:
 		print(e)
 	return none
-
-def getCustomers(conn):
-	cur = conn.cursor()
-	cur.execute("SELECT ID, CompanyName FROM Customer")
-	table = cur.fetchall()
-
-	print("ID", "\t", "Customer Name")
-	for row in table:
-		print(str(row[0]), "\t", str(row[1]))
-
-def getEmployees(conn):
-	cur = conn.cursor()
+def employeesdata(connection):
+	cur = connection.cursor()
 	cur.execute("SELECT ID, FirstName, LastName FROM Employee")
 	table = cur.fetchall()
 	print("ID", "\t", "Employee Name")
 	for row in table:
 		print(str(row[0]) + "\t" + str(row[1]) + " " + str(row[2]))
-
-def getOrdersByCID(conn, CID):
-	cur = conn.cursor()
-	cur.execute("SELECT OrderDate FROM \"Order\" WHERE CustomerId=?", (CID,))
+def customerdata(connection):
+	cur = connection.cursor()
+	cur.execute("SELECT ID, CompanyName FROM Customer")
 	table = cur.fetchall()
-	print("OrderDate")
+	print("ID", "\t", "Customer Name")
 	for row in table:
-		print(str(row[0]))
-
-def getOrderByEmpLN(conn, EmpLN):
-	cur = conn.cursor()
+		print(str(row[0]), "\t", str(row[1]))
+def orderEmpLN(connection, EmpLN):
+	cur = connection.cursor()
 	cur.execute('''SELECT OrderDate FROM "Order", "Employee" WHERE "Order".EmployeeId="Employee".Id AND "Employee".LastName="''' + str(EmpLN) + "\"")
 	table = cur.fetchall()
 	print("OrderDate")
 	for row in table:
 		print(str(row[0]))
-
+def CIDdata(connection, CID):
+	cur = connection.cursor()
+	cur.execute("SELECT OrderDate FROM \"Order\" WHERE CustomerId=?", (CID,))
+	table = cur.fetchall()
+	print("OrderDate")
+	for row in table:
+		print(str(row[0]))
 def main():
 	database = "./Northwind_small.sqlite"
 	#Establishing a Database connection
-	conn = createConnection(database)
+	conn = connection(database)
 	if(len(sys.argv)>1):
 		with conn:
 			if(sys.argv[1] == "customers"):
-				getCustomers(conn)
-
+				customerdata(conn)
 			elif(sys.argv[1]=="employees"):
-				getEmployees(conn)
-
+				employeesdata(conn)
 			elif(sys.argv[1] =="orders"):
 				if(sys.argv[2][0] == "c" or sys.argv[2][0] == "C"):
-					getOrdersByCID(conn, sys.argv[2][5:])
+					CIDdata(conn, sys.argv[2][5:])
 				elif(sys.argv[2][0] == "e" or sys.argv[2][0] == "E"):
-					getOrderByEmpLN(conn, sys.argv[2][4:])
+					orderEmpLN(conn, sys.argv[2][4:])
 				else:
 					print("Please enter a valid argument")
 	else:
-		print("No valid argument found, please try again")
-
+		print("Error")
 main()
