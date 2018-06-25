98df70a2c165ab8624132a7cf5a8f71a6fb59fa4
Kesava Karthik Kota <35501783+kesavakk@users.noreply.github.com>
2018-06-24 23:57:48 -0400

    Add files via upload
    gfyguo

--- part1.py
+++ part1.py
@@ -6,16 +6,12 @@ consumer_key = "fzTKr5UJm5JEYdtMGo4359QYJ"
 consumer_secret = "aVo6QqSjjNe18n3YW2uEsjEOovNMvFlrZYpGJK3WrWjeKLrX05"
 access_token = "2470112398-BdgcMHVcYLBJkMYvDTNBQWuJYhCQC223ZxUQB4S"
 access_token_secret = "UB8ANuStsE6GiRkOiKBlG8QIQm6WpJhLUQ6iuuPVaGkKs"
-
 stopWords = ['http','https','RT']
-
-filteredTokens = []
-
+suitableTokens = []
 f = open('noun_data.csv','w')
 f.write('Noun,Number\n')
 
-
-def printToken(figofspeech, writecommafile):
+def printwords(figofspeech, writecommafile):
     fig_dict = {}
     count = 0
     for v in figofspeech:
@@ -23,7 +19,6 @@ def printToken(figofspeech, writecommafile):
             fig_dict[v]+=1
         else:
             fig_dict[v]=1
-
     finalString= ""
     for (word, tag) in sorted(fig_dict.items(), key=lambda x: (-x[1], x[0])):
         if count < 5:
@@ -42,32 +37,23 @@ def main():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-
-
+    retweetcount = 0
     user = sys.argv[1]
     numOfTweets = sys.argv[2]
-
-    favs = 0
-    rtCount = 0
-
+    favorites = 0
     tweets = api.user_timeline(screen_name = user,count = numOfTweets,include_rts=False)
-
     originalTweets = len(tweets)
-
-
-
     for tweet in tweets:
-        rtCount += tweet.retweet_count
-        favs += tweet.favorite_count
+        retweetcount += tweet.retweet_count
+        favorites += tweet.favorite_count
         tweet_tokens = nltk.word_tokenize(tweet.text)
         for w in tweet_tokens:
             if (w[0].isalpha()) and (w not in stopWords) and (len(w)>1):
-                filteredTokens.append(w)
+                suitableTokens.append(w)
+    adjectives = []
     verbs = []
     nouns = []
-    adjectives = []
-
-    for word,tag in nltk.pos_tag(filteredTokens):
+    for word,tag in nltk.pos_tag(suitableTokens):
         if tag[0] == "V" and tag[1] == "B":
             verbs.append(word)
         elif tag[0] == "N" and tag[1] == "N":
@@ -75,21 +61,12 @@ def main():
         elif tag[0] == "J" and tag[1] == "J":
             adjectives.append(word)
     print("USER:", user)
-    if str(numOfTweets) > str(200):
-        print("TWEETS ANALYZED: 200")
-    else:
-        print("TWEETS ANALYZED:", numOfTweets)
-    print("VERBS:", printToken(verbs, False))
-    print("Nouns:", printToken(nouns, True))
+    print("TWEETS ANALYZED:", numOfTweets)
+    print("VERBS: ", printwords(verbs, False))
+    print("NOUNS: ", printwords(nouns, True))
     f.close()
-    print("Adjectives:", printToken(adjectives,False))
-    print("ORIGINAL TWEETS:", originalTweets)
-    print("TIMES FAVORITED (ORIGINAL TWEETS ONLY):", favs)
-    print("TIMES RETWEETED (ORIGINAL TWEETS ONLY):", rtCount)
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+    print("ADJECTIVES: ", printwords(adjectives,False))
+    print("ORIGINAL TWEETS: ", originalTweets)
+    print("TIMES FAVORITED (ORIGINAL TWEETS ONLY): ", favorites)
+    print("TIMES RETWEETED (ORIGINAL TWEETS ONLY): ", retweetcount)
+main()
